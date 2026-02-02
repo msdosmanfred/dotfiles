f9c8846f89a2402ff7a972ac5cd56f8e6a7550f0
msdosmanfred <hoefer9@gmail.com>
2026-02-02 14:25:43 +0100

    updated dotfiles

---
+++ config/fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+#
+#
+alias vim='nvim'
+starship init fish | source
